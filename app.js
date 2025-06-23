3d07d0973f0aa13ebf948d6b544d4c117b34fa98
Vitor <vitorfernandes11122006@gmail.com>
2025-06-23 09:19:05 -0300

    Criação da página de login

---
+++ app.js
@@ -0,0 +1,15 @@
+const entrarClick = document.getElementById('entrarbtn')
+
+entrarClick.addEventListener('click', function(event){
+    event.preventDefault()
+    let senha = document.getElementById('senha').value;
+    let login = document.getElementById('login').value;
+    let errata = document.getElementById('acessFail'); 
+
+    if (login == 'admin' && senha == 'sccp@1910'){
+    window.location.href = 'home.html'}
+    else{
+        errata.style.display = 'flex';
+    }
+
+});
