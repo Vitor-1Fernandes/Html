f9915b0c9ac4170eb7bda081e560dd43fe6e97bf
Vitor <vitorfernandes11122006@gmail.com>
2025-06-26 16:39:27 -0300

    Criação de loops para coleta e preenchimento dos dados

--- python/protheus.py
+++ python/protheus.py
@@ -4,55 +4,77 @@ import time
 import keyboard
 import pygetwindow as gw
 
-# planilha = load_workbook('credenciais.xlsx')
-# pagina = planilha['Plan1']
+planilha = load_workbook("C:\\Users\\vitor.santos\\Documents\\Estudos\\Python\\Automação\\Html\\python\\produtos - Copia.xlsx", data_only=True)
+pagina = planilha['Plan1']
 
-# produto = []
-# quantidade = []
-# armazen = []
-# documento = 123456z
-# for dados in pagina.iter_rows(values_only=True):
-#     produto.append(dados[0])
-#     quantidade.append(dados[1])
-#     armazen.append(dados[2])
+produtolist = []
+qtdlist = []
+armazenlist = []
+documento = 123456
+for dados in pagina.iter_rows(values_only=True):
+    produtolist.append(dados[0])
+    qtdlist.append(dados[1])
+    armazenlist.append(dados[2])
+
+print(f"{produtolist} \n \n{qtdlist} \n \n{armazenlist}")
 
 def clicar(vezes, tecla):
     i = 0
     while i < vezes: 
+        time.sleep(0.01)
         keyboard.send(tecla)
         i = i + 1  
 
-keyboard.send('windows')
+def logarApp(user, senha, app):
 
-time.sleep(1)
-pyautogui.write("smartview")
+    keyboard.send('windows')
 
-time.sleep(1)
-keyboard.send('enter')
+    time.sleep(0.5)
+    pyautogui.write(app)
 
-time.sleep(1)
-keyboard.send('tab')
-pyautogui.write("admin")
+    time.sleep(1)
+    keyboard.send('enter')
 
-time.sleep(1)
-keyboard.send('tab')
-pyautogui.write("sccp@1910")
+    time.sleep(2)
+    keyboard.send('tab')
+    pyautogui.write(user)
 
-time.sleep(3)
-keyboard.send('tab')
-keyboard.send('enter')
+    time.sleep(0.5)
+    keyboard.send('tab')
+    pyautogui.write(senha)
 
-time.sleep(3)
-clicar(3, 'tab')
-pyautogui.write('721914')
-
-clicar(1, 'tab')
-pyautogui.write("1")
-
-clicar(5, 'tab')
-pyautogui.write("03")
-
-clicar(10, 'tab')
-pyautogui.write("45788")
+    time.sleep(0.5)
+    keyboard.send('tab')
+    keyboard.send('enter')
 
 
+def salvarProdutos(produto, qtd, armazen, doc, nº):
+    time.sleep(1)
+    if nº == 0:
+        clicar(3, 'tab')
+    else:
+        clicar(1, 'tab')
+    pyautogui.write(produto)
+
+    clicar(1, 'tab')
+    pyautogui.write(qtd)
+
+    clicar(5, 'tab')
+    pyautogui.write(armazen)
+
+    clicar(12, 'tab')
+    pyautogui.write(doc)
+
+    clicar(8, 'tab')
+    clicar(1, 'enter')
+
+appName = "\"C:\\Program Files\\Google\\Chrome\\Application\\chrome_proxy.exe\" --profile-directory=\"Profile 1\" --app-id=ginkifbklpdfedpooabndoadkohlhndl"
+
+logarApp(
+    "admin",
+    "sccp@1910",
+    appName
+)
+
+for i in range(len(produtolist)):
+    salvarProdutos(str(int(produtolist[i])), str(int(qtdlist[i])), str(armazenlist[i]), str(documento), i)
